0a0abf7a242aa771be41fd9d8e18055b30cb4210
atmarksharp <atmarksharp@live.jp>
2017-11-20 17:43:55 +0900

    名詞の派生系を追加

--- scripts/jmdict_to_nums_map.py
+++ scripts/jmdict_to_nums_map.py
@@ -11,20 +11,50 @@ f = open(dirname + "/../data/dict/jmdict_lemma_single.json",'r',encoding="utf-8_
 jmdict = json.load(f)
 f.close()
 
+def print_json_element(dic):
+	d = dic
+	print( "{" )
+	if( d["kanji"] != None ):
+		print( '"kanji": "' + d["kanji"] + '",' )
+	print( '"nums": "' + d["nums"]  + '",' )
+	print( '"kana": "' + d["kana"] + '",' )
+	print( '"hira": "' + d["hira"] + '",' )
+	print( '"pos": ' + str(d["pos"]).replace("'", '"') + ',' )
+	if( "vari" in d ):
+		print( '"vari": "' + d["vari"] + '"' )
+	print( "}," )
+
+print("[")
+
 count = 0
 for entry in jmdict:
 	if("reb" in entry):
-		reb = entry["reb"]
-		kana = jaconv.kata2hira( reb )
+		kanji = None
+		if( "keb" in entry ):
+			kanji = entry["keb"]
+		kana = entry["reb"]
+		hira = jaconv.kata2hira( kana )
 		pos = entry["pos"]
-		nums = kana2nums(kana)
+		nums = kana2nums(hira)
 		if(nums != None):
-			print( "{" )
-			if( "keb" in entry ):
-				print( '"kanji": "' + entry["keb"] + '",' )
-			print( '"kana": "' + reb + '",' )
-			print( '"hira": "' + kana + '",' )
-			print( '"pos": ' + str(pos).replace("'", '"') + ',' )
-			print( '"nums": "' + nums  + '"' )
-			print( "}," )
+			e = {
+				"kanji": kanji,
+				"kana": kana,
+				"hira": hira,
+				"pos": pos,
+				"nums": nums
+			}
+
+			print_json_element(e)
 			count += 1
+
+			if "n" in pos:
+				e["vari"] = "1"
+				if( kanji != None ):
+					e["kanji"] = e["kanji"] + "に"
+				e["hira"] += "に"
+				e["kana"] += "に"
+				e["nums"] += "2"
+				print_json_element(e)
+
+print("]")
